4f879dfa4c7a2f092ee768642c80a7ab7e2987bb
yervandsargsyan <84045761+yervandsargsyan@users.noreply.github.com>
2026-02-17 17:27:08 +0400

    comments in train_bpe_corpus.py

--- scripts/train_bpe_corpus.py
+++ scripts/train_bpe_corpus.py
@@ -2,19 +2,19 @@ from tokenizer.bpe_trainer import BPETrainer
 from scripts.save_tokenizer import save_tokenizer
 
 def main():
-    # 1️⃣ Read corpus
+    # Read corpus
     with open("data/corpus.txt", "r", encoding="utf-8") as f:
         text = f.read()
 
     print("Corpus length:", len(text))
 
-    # 2️⃣ Train BPE tokenizer
+    # Train BPE tokenizer
     trainer = BPETrainer()
     tokenizer = trainer.train(text, vocab_size=1000)  # можно увеличить vocab_size
 
     print("Tokenizer trained. Number of merges:", len(tokenizer.merges))
 
-    # 3️⃣ Save tokenizer
+    # Save tokenizer
     save_tokenizer(tokenizer, path="tokenizer.json")
     print("Tokenizer saved to tokenizer.json")
 
